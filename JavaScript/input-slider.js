4158ec435587f2914214bff91783dbeb57c6d4e8
FlameWolf <agnijith@gmail.com>
2019-11-18 20:22:35 +0530

    Update JavaScript/input-slider.js

--- JavaScript/input-slider.js
+++ JavaScript/input-slider.js
@@ -2,35 +2,43 @@
 customElements.define("input-slider", class extends HTMLElement {
 	constructor() {
 		super();
-		this.label = document.createElement("label");
-		this.input = document.createElement("input");
-		this.outputHolder = document.createElement("span");
+		Object.defineProperties(this, {
+			"label": { "value": document.createElement("label") },
+			"input": { "value": document.createElement("input") },
+			"outputHolder": { "value": document.createElement("span") },
+			"button": { "value": document.createElement("button") },
+			"valueSuffix": { "value": new String(), "writable": true },
+			"dirty": { "value": false, "writable": true },
+			"position": {
+				set(value) {
+					this.input.value = value;
+					this.input.dispatchEvent(new Event("input"));
+				},
+				get() {
+					return this.input.value;
+				}
+			},
+			"reset": {
+				"value": function() {
+					this.dirty = false;
+					this.position = this.getAttribute("default");
+				}
+			}
+		});
+		Object.seal(this);
 		this.outputHolder.style.float = "right";
-		this.button = document.createElement("button");
 		this.button.style.float = "right";
-		this.valueSuffix = new String();
-		this.dirty = false;
-	}
-	set position(value) {
-		this.input.value = value;
-		this.input.dispatchEvent(new Event("input"));
-	}
-	get position() {
-		return this.input.value;
-	}
-	reset() {
-		this.dirty = false;
-		this.position = this.getAttribute("default");
+		this.attachShadow({ "mode": "open" });
 	}
 	static get observedAttributes() {
-		return [ "text", "id", "min", "max", "default", "value-suffix" ];
+		return [ "text", "name", "min", "max", "default", "value-suffix" ];
 	}
 	attributeChangedCallback(attrName, oldValue, newValue) {
 		switch(attrName) {
 			case "text":
 				this.label.textContent = newValue;
 				break;
-			case "id":
+			case "name":
 				this.label.setAttribute("for", newValue);
 				this.input.setAttribute("id", newValue);
 				break;
@@ -53,11 +61,10 @@ customElements.define("input-slider", class extends HTMLElement {
 	}
 	connectedCallback() {
 		if(this.isConnected) {
-			let shadow = this.attachShadow({ "mode": "open" });
 			let container = document.createElement("p");
-			this.label.setAttribute("for", this.getAttribute("id"));
+			this.label.setAttribute("for", this.getAttribute("name"));
 			this.label.textContent = this.getAttribute("text");
-			this.input.setAttribute("id", this.getAttribute("id"));
+			this.input.setAttribute("name", this.getAttribute("name"));
 			this.input.setAttribute("type", "range");
 			this.input.setAttribute("min", this.getAttribute("min"));
 			this.input.setAttribute("max", this.getAttribute("max"));
@@ -78,9 +85,8 @@ customElements.define("input-slider", class extends HTMLElement {
 			container.appendChild(document.createElement("br"));
 			container.appendChild(this.input);
 			container.appendChild(this.button);
-			shadow.appendChild(container);
-			container = null
-			shadow = null;
+			this.shadowRoot.appendChild(container);
+			container = null;
 		}
 	}
 });
