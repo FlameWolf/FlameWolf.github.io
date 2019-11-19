63e32e35075a39c18b078b4a2d6e52dfedf08504
FlameWolf <agnijith@gmail.com>
2019-11-19 13:52:38 +0530

    Update JavaScript/input-slider.js

--- JavaScript/input-slider.js
+++ JavaScript/input-slider.js
@@ -3,12 +3,32 @@ customElements.define("input-slider", class extends HTMLElement {
 	constructor() {
 		super();
 		Object.defineProperties(this, {
-			"label": { "value": document.createElement("label") },
-			"input": { "value": document.createElement("input") },
-			"outputHolder": { "value": document.createElement("span") },
-			"button": { "value": document.createElement("button") },
-			"valueSuffix": { "value": new String(), "writable": true },
-			"dirty": { "value": false, "writable": true },
+			"label": {
+				"value": document.createElement("label"),
+				"enumerable": true
+			},
+			"input": {
+				"value": document.createElement("input"),
+				"enumerable": true
+			},
+			"outputHolder": {
+				"value": document.createElement("span"),
+				"enumerable": true
+			},
+			"button": {
+				"value": document.createElement("button"),
+				"enumerable": true
+			},
+			"valueSuffix": {
+				"value": new String(),
+				"writable": true,
+				"enumerable": true
+			},
+			"dirty": {
+				"value": false,
+				"writable": true,
+				"enumerable": true
+			},
 			"position": {
 				set(value) {
 					this.input.value = value;
@@ -16,19 +36,18 @@ customElements.define("input-slider", class extends HTMLElement {
 				},
 				get() {
 					return this.input.value;
-				}
-			},
-			"reset": {
-				"value": function() {
-					this.dirty = false;
-					this.position = this.getAttribute("default");
-				}
+				},
+				"enumerable": true
 			}
 		});
 		Object.seal(this);
 		this.outputHolder.style.float = "right";
 		this.button.style.float = "right";
 		this.attachShadow({ "mode": "open" });
+	}
+	reset() {
+		this.dirty = false;
+		this.position = this.getAttribute("default");
 	}
 	static get observedAttributes() {
 		return [ "text", "name", "min", "max", "default", "value-suffix" ];
